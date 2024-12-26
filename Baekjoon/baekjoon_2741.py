04c47b44a9efa77a6c0c53b2a59f7b8aedfade5e
JAEHWAN <woghks1213@gmail.com>
2024-12-26 16:43:26 +0900

    Add: 241226 baekjoon_2741 [N 찍기] solved

---
+++ Baekjoon/baekjoon_2741.py
@@ -0,0 +1,12 @@
+# N 찍기 
+# https://www.acmicpc.net/problem/2741
+
+def main():
+    N = int(input())
+    arr = [i for i in range(1, N+1)]
+    for n in arr:
+        print(n)
+    return 
+
+if __name__=='__main__':
+    main()
