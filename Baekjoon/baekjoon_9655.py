fe1daca571fdc301006ed39410329f2d1214e311
JAEHWAN <woghks1213@gmail.com>
2025-02-04 20:39:30 +0900

    Add: 250204 baekjoon_9655 [돌 게임] solved

---
+++ Baekjoon/baekjoon_9655.py
@@ -0,0 +1,15 @@
+# 돌게임
+# https://www.acmicpc.net/problem/9655
+
+def main():
+
+    N = int(input())
+
+    if N % 2 == 0:
+        print("CY")
+    else:
+        print("SK")
+    return
+
+if __name__=='__main__':
+    main()
