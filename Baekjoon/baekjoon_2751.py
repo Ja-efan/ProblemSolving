6f119425530796af84f9a1eda520afe246ec3412
JAEHWAN <woghks1213@gmail.com>
2025-03-07 09:30:37 +0900

    solved: 250307 baekjoon_2751 [수 정렬하기 2]

--- Baekjoon/baekjoon_2751.py
+++ Baekjoon/baekjoon_2751.py
@@ -2,35 +2,38 @@
 # quick sort 
 
 import sys 
-sys.setrecursionlimit(10000)
+inputf = sys.stdin.readline
+printf = sys.stdout.write
 
-def split(arr, l, r):
-    pivot = arr[r]  # 가장 오른쪽 원소로 피봇팅 
-    i = l - 1
-    for j in range(l, r):
-        if arr[j] <= pivot:
-            i += 1
-            arr[i], arr[j] = arr[j], arr[i]
+# def split(arr, l, r):
+#     pivot = arr[r]  # 가장 오른쪽 원소로 피봇팅 
+#     i = l - 1
+#     for j in range(l, r):
+#         if arr[j] <= pivot:
+#             i += 1
+#             arr[i], arr[j] = arr[j], arr[i]
 
-    arr[i+1], arr[r] = arr[r], arr[i+1]
-    return i + 1 
+#     arr[i+1], arr[r] = arr[r], arr[i+1]
+#     return i + 1 
 
-def quicksort(arr, l, r):
-    if l < r:
-        pivot_idx = split(arr, l, r)
-        quicksort(arr, l, pivot_idx-1)
-        quicksort(arr, pivot_idx+1, r)
+# def quicksort(arr, l, r):
+#     if l < r:
+#         pivot_idx = split(arr, l, r)
+#         quicksort(arr, l, pivot_idx-1)
+#         quicksort(arr, pivot_idx+1, r)
 
 def main():
-    N = int(sys.stdin.readline())
-    arr = [0 for _ in range(N)]
+
+    N = int(inputf())
+    arr = list()
+    for _ in range(N):
+        arr.append(int(inputf()))
+    
+    arr_sorted = sorted(arr)
 
     for i in range(N):
-        # arr[i] = int(input())
-        arr[i] = int(sys.stdin.readline())
-    quicksort(arr, 0, N-1)
-
-    print(*arr, sep="\n")
+        printf(str(arr_sorted[i])+'\n')
+    # printf(*arr_sorted, sep="\n")
     
     return
 
